5fe8e033a54f4af128b499a9e622c33ea4340651
JakPaww <mfdzaky06@gmail.com>
2024-12-17 12:40:41 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+document.title = 'Coba'
+const body = document.body;
+const h1 = document.createElement('h1');
+h1.innerHTML = '<marquee>Tes aja bro</marquee>'
+body.append(h1)
+
+
+function save() {
+    body.append(' tesss aja ')
+    Text.length.toFixed
+}
